129a2400393733eca8730c9c1c2d09d64fac9540
TechieTeee <techieteee@protonmail.com>
2023-07-07 02:53:55 +0000

    Updated code with weavedb instance, and also making predictions on individual Lens Protocol profiles

--- WeaveML Library/LensFMLWeaveDB.py
+++ WeaveML Library/LensFMLWeaveDB.py
@@ -1,26 +1,43 @@
-import json
-import requests
 import weavedb
-import flowers_fml
+import requests
+import json
+import flowers
 import xgboost
 
-class LensFMLWeaveDB(weavedb.WeaveDB):
+def predict(data):
+    model = flowers.load("<path-to-machine-learning-model>")
+    xgboost_model = xgboost.Booster()
+    xgboost_model.load_model(model_data)
+    return xgboost_model.predict(data)
 
-    def save_model(self, model):
-        model_data = flowers_fml.serialize_model(model)
-        xgboost_model = xgboost.Booster()
-        xgboost_model.load_model(model_data)
-        self.save_data(xgboost_model)
+def save_model(model):
+    model_data = flowers_fml.serialize_model(model)
+    xgboost_model = xgboost.Booster()
+    xgboost_model.load_model(model_data)
+    db.save_data(xgboost_model)
 
-    def load_model(self):
-        xgboost_model = self.load_data()
-        return flowers_fml.deserialize_model(xgboost_model)
+def load_model():
+    xgboost_model = db.load_data()
+    return flowers_fml.deserialize_model(xgboost_model)
 
-    def get_lens_data(self):
-        url = "https://api.thegraph.com/subgraphs/name/lensprotocol/lens"
-        response = requests.get(url)
-        data = json.loads(response.content)
-        return data
+if name == "__main__":
+    db = weavedb.WeaveDB("v7xdnco4ygYpqCCCqGo7RYkRqoieEFxC-kS63UsfWXA")
+
+    url = "https://api.thegraph.com/subgraphs/name/lensprotocol/lens"
+    response = requests.get(url)
+    data = json.loads(response.content)
+
+    db.insert(data)
+
+    model = xgboost.Booster()
+    model.fit(data["features"], data["target"])
+    save_model(model)
+    model = load_model()
+    predictions = []
+    for record in data:
+        predictions.append(predict(record))
+
+    print(predictions)
 
 if name == "__main__":
     db = LensFMLWeaveDB()
