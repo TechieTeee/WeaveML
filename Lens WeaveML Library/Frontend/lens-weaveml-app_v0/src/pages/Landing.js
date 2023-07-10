0230d91e080b83009b371993c08b3a7cce893aa0
_earthtospace <niema.widaha@gmail.com>
2023-07-10 16:39:06 -0700

    frontend

---
+++ Lens WeaveML Library/Frontend/lens-weaveml-app_v0/src/pages/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css';
+
+const Landing = () => {
+  return (
+    <div className='p-10'>
+        <p className='justify-center text-center text-2xl'> 
+          Lens Weave ML Library
+        </p>
+    </div>
+  )
+}
+
+export default Landing
