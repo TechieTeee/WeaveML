7e84573541e591599ae5a9112d2efd852db7a351
TechieTeee <techieteee@protonmail.com>
2023-07-10 22:17:06 +0000

    Updated functions for storing model and using API key and using .env file

--- Lens WeaveML Library/Backend/Lens_FML_WeaveDB.js
+++ Lens WeaveML Library/Backend/Lens_FML_WeaveDB.js
@@ -1,47 +1,77 @@
 const tf = require('@tensorflow/tfjs');
 const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+
+const weavedb = {
+  WeaveDB: function(apiKey) {
+    // For WeaveDB constructor
+    this.apiKey = apiKey;
+  },
+  saveData: function(data) {
+    // For saving data to WeaveDB
+    console.log('Saving data:', data);
+  },
+  loadData: function() {
+    // For loading data from WeaveDB
+    const data = {}; // Example data retrieved from storage
+    console.log('Loading data:', data);
+    return data;
+  }
+};
+
+const requests = {
+  get: async function(url) {
+    // Implementation for making a GET request
+    const response = await fetch(url);
+    const content = await response.text();
+    return { content };
+  }
+};
 
 const predict = async (data, model) => {
-    return model.predict(data);
+  return model.predict(data);
 };
 
 const saveModel = async (model) => {
-    const modelData = await model.toJSON();
-    // Save modelData using db.saveData()
+  const modelData = await model.toJSON();
+  // Save modelData using db.saveData()
+  db.saveData(modelData);
 };
 
 const loadModel = async () => {
-    const modelData = await db.loadData();
-    const model = await tf.loadLayersModel(tf.io.fromJSON(modelData));
-    return model;
+  const modelData = await db.loadData();
+  const model = await tf.loadLayersModel(tf.io.fromJSON(modelData));
+  return model;
 };
 
 async function main() {
-    const db = weavedb.WeaveDB("v7xdnco4ygYpqCCCqGo7RYkRqoieEFxC-kS63UsfWXA");
+  dotenv.config(); // Load environment variables from .env file
 
-    const url = "https://thegraph.com/hosted-service/subgraph/anudit/lens-protocol";
-    const response = await fetch(url);
-    const data = await response.json();
+  const db = new weavedb.WeaveDB(process.env.API_KEY);
+  const url = process.env.GRAPH_API_URL;
 
-    db.insert(data);
+  const response = await requests.get(url);
+  const data = JSON.parse(response.content);
 
-    const model = tf.sequential();
-    model.add(tf.layers.dense({ units: 1, inputShape: [data["features"].length] }));
-    model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
-    const xs = tf.tensor(data["features"]);
-    const ys = tf.tensor(data["target"]);
-    await model.fit(xs, ys, { epochs: 10 });
-    await saveModel(model);
+  db.insert(data);
 
-    const loadedModel = await loadModel();
-    const predictions = [];
-    for (const record of data) {
-        const input = tf.tensor(record);
-        const prediction = await predict(input, loadedModel);
-        predictions.push(prediction);
-    }
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ units: 1, inputShape: [data["features"].length] }));
+  model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+  const xs = tf.tensor(data["features"]);
+  const ys = tf.tensor(data["target"]);
+  await model.fit(xs, ys, { epochs: 10 });
+  await saveModel(model);
 
-    console.log(predictions);
+  const loadedModel = await loadModel();
+  const predictions = [];
+  for (const record of data) {
+    const input = tf.tensor(record);
+    const prediction = await predict(input, loadedModel);
+    predictions.push(prediction);
+  }
+
+  console.log(predictions);
 }
 
 main();
