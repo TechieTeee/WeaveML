f9313d8d79f230bde13245db9bc29bbdd49ebc2a
_earthtospace <niema.widaha@gmail.com>
2023-07-10 16:49:06 -0700

    frontend updates

--- Lens WeaveML Library/Frontend/lens-weaveml-app_v0/src/pages/Landing.js
+++ Lens WeaveML Library/Frontend/lens-weaveml-app_v0/src/pages/Landing.js
@@ -3,10 +3,11 @@ import '../App.css';
 
 const Landing = () => {
   return (
-    <div className='p-10'>
-        <p className='justify-center text-center text-2xl'> 
+    <div className='p-10 flex justify-center items-center'>
+        <p className='text-2xl'> 
           Lens Weave ML Library
         </p>
+        {/* <img src={"https://i.ibb.co/mGtcy3j/twitter-OG.png"} className='flex mt-10 w-1/2' alt="lens logo"></img> */}
     </div>
   )
 }
